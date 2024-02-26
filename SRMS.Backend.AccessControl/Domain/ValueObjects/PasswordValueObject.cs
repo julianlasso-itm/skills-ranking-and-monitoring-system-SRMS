9727b3942839815adcfca507ec9339a86ab4fb1d
Julian Lasso <julianlasso1133109@correo.itm.edu.co>
2024-02-26 00:56:48 -0500

    fix(backend): :bug: when the password is obtained, the password is encrypted

--- SRMS.Backend.AccessControl/Domain/ValueObjects/PasswordValueObject.cs
+++ SRMS.Backend.AccessControl/Domain/ValueObjects/PasswordValueObject.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography;
+using System.Text;
 using System.Text.RegularExpressions;
 using SRMS.Shared.Domain.ValueObjects;
 
@@ -5,6 +7,7 @@ namespace SRMS.Backend.AccessControl.Domain.ValueObjects;
 
 public class PasswordValueObject : StringValueObject
 {
+    public String Value { get => CreateSha512Hash(base.Value); }
     const int MinLength = 8;
     const string PasswordRegex = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$";
 
@@ -35,5 +38,21 @@ public class PasswordValueObject : StringValueObject
     private bool IsPasswordValid(string value)
     {
         return Regex.IsMatch(value, PasswordRegex);
+    }
+
+    private String CreateSha512Hash(String value)
+    {
+        using (SHA512 sha512 = SHA512.Create())
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(value);
+            byte[] hash = sha512.ComputeHash(bytes);
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                stringBuilder.Append(hash[i].ToString("X2"));
+            }
+            return stringBuilder.ToString();
+        }
+
     }
 }
