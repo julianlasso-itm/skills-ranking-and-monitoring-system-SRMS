149d4020f5f11b8a9753f978e37ee93de98d67e0
Julian Lasso <julianlasso1133109@correo.itm.edu.co>
2024-04-28 06:49:46 -0500

    feat(backend): :sparkles: add helper classes for deleting cities, countries, roles, and skills

---
+++ src/Backend/Profiles/Infrastructure/Services/Helpers/Base/BaseHelperServiceInfrastructure.cs
@@ -0,0 +1,31 @@
+using Profiles.Application;
+using Profiles.Infrastructure.Persistence.Models;
+
+namespace Profiles.Infrastructure.Services.Helpers.Base
+{
+    public abstract class BaseHelperServiceInfrastructure
+    {
+        protected static Application<
+            CountryModel,
+            ProvinceModel,
+            CityModel,
+            RoleModel,
+            SkillModel,
+            ProfessionalModel
+        > Application;
+
+        public static void SetApplication(
+            Application<
+                CountryModel,
+                ProvinceModel,
+                CityModel,
+                RoleModel,
+                SkillModel,
+                ProfessionalModel
+            > application
+        )
+        {
+            Application = application;
+        }
+    }
+}
