44f1535eab85ea85365acab783b2b7ee57ef3937
Veronica Gaviria G <veronicagaviria1126235@correo.itm.edu.co>
2024-04-28 07:01:08 -0500

    ✨ api controllers and services are added

---
+++ src/Backend/ApiGateway/Infrastructure/Services/AccessControlService.cs
@@ -0,0 +1,50 @@
+using ApiGateway.Infrastructure.Services.Base;
+using ProtoBuf.Grpc;
+using Shared.Infrastructure.ProtocolBuffers.AccessControl;
+using Shared.Infrastructure.ProtocolBuffers.AccessControl.Requests;
+using Shared.Infrastructure.ProtocolBuffers.AccessControl.Responses;
+
+namespace ApiGateway.Infrastructure.Services;
+
+public class AccessControlService : BaseServices<IAccessControlServices>, IAccessControlServices
+{
+    const string UrlMicroservice = "http://localhost:5297";
+
+    public AccessControlService(HttpClientHandler? httpClientHandler = null)
+        : base(httpClientHandler)
+    {
+        CreateChannel(UrlMicroservice);
+    }
+
+    public Task<RegisterRoleSecurityResponse> RegisterRoleAsync(
+        RegisterRoleSecurityRequest request,
+        CallContext context = default
+    )
+    {
+        return Client.RegisterRoleAsync(request, context);
+    }
+
+    public Task<UpdateRoleSecurityResponse> UpdateRoleAsync(
+        UpdateRoleSecurityRequest request,
+        CallContext context = default
+    )
+    {
+        return Client.UpdateRoleAsync(request, context);
+    }
+
+    public Task<DeleteRoleSecurityResponse> DeleteRoleAsync(
+        DeleteRoleSecurityRequest request,
+        CallContext context = default
+    )
+    {
+        return Client.DeleteRoleAsync(request, context);
+    }
+
+    public Task<GetRolesSecurityResponse> GetRolesAsync(
+        GetRolesSecurityRequest request,
+        CallContext context = default
+    )
+    {
+        return Client.GetRolesAsync(request, context);
+    }
+}
