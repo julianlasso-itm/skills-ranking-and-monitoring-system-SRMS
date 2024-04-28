b1db0354e9e1205e7e9ad6111d5e0d90ff06a343
Julian Lasso <julianlasso1133109@correo.itm.edu.co>
2024-04-28 07:47:01 -0500

    feat(frontend): :sparkles: add animation support to app configuration and update app routes

--- src/Frontend/src/app/app.routes.ts
+++ src/Frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./modules/admin/admin.module').then((m) => m.AdminModule),
+  },
+  {
+    path: 'profiles',
+    loadChildren: () =>
+      import('./modules/profiles/profiles.module').then(
+        (m) => m.ProfilesModule
+      ),
+  },
+];
