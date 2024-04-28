fb3ca6dcfd52b82439a3f9f1437a46fefe421f7b
Veronica Gaviria G <veronicagaviria1126235@correo.itm.edu.co>
2024-04-28 06:59:35 -0500

    ✨ repositories migrations are generated to acces control

---
+++ src/Backend/AccessControl/Infrastructure/Persistence/Models/RoleModel.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using Shared.Infrastructure.Persistence.Models;
+
+namespace AccessControl.Infrastructure.Persistence.Models;
+
+[Index(nameof(Name), IsUnique = true)]
+[Table("role")]
+public class RoleModel : AuditableEntity
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    [Column("rol_role_id")]
+    public Guid RoleId { get; set; }
+
+    [Required]
+    [Column("rol_name")]
+    [MaxLength(100)]
+    public string Name { get; set; }
+
+    [Column("rol_description")]
+    [MaxLength(1024)]
+    public string? Description { get; set; }
+
+    [Required]
+    [Column("rol_disabled")]
+    public bool Disabled { get; set; }
+}
