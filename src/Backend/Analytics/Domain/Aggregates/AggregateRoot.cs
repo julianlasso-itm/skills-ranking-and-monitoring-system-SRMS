5cfe9397f4d7a75ce53d863f42fe0fcb5c7dd7ac
Veronica Gaviria G <veronicagaviria1126235@correo.itm.edu.co>
2024-04-28 07:02:43 -0500

    ✨ domain is added for analytics

---
+++ src/Backend/Analytics/Domain/Aggregates/AggregateRoot.cs
@@ -0,0 +1,29 @@
+using Analytics.Domain.Aggregates.Dto.Requests;
+using Analytics.Domain.Aggregates.Dto.Responses;
+using Analytics.Domain.Aggregates.Helpers;
+using Analytics.Domain.Aggregates.Interfaces;
+using Shared.Domain.Aggregate.Base;
+using Shared.Domain.Events.Interfaces;
+
+namespace Analytics.Domain.Aggregates;
+
+public class AggregateRoot : BaseAggregateRoot, IAggregateRoot
+{
+    public AggregateRoot(IEvent eventInterface)
+        : base(eventInterface) { }
+
+    public DeleteLevelDomainResponse DeleteLevel(DeleteLevelDomainRequest request)
+    {
+        return DeleteLevelHelper.Execute(request);
+    }
+
+    public RegisterLevelDomainResponse RegisterLevel(RegisterLevelDomainRequest request)
+    {
+        return RegisterLevelHelper.Execute(request);
+    }
+
+    public UpdateLevelDomainResponse UpdateLevel(UpdateLevelDomainRequest request)
+    {
+        return UpdateLevelHelper.Execute(request);
+    }
+}
---
+++ src/Backend/Analytics/Domain/Aggregates/Helpers/UpdateLevelHelper.cs
@@ -0,0 +1,73 @@
+using Analytics.Domain.Aggregates.Dto.Requests;
+using Analytics.Domain.Aggregates.Dto.Responses;
+using Analytics.Domain.Entities;
+using Analytics.Domain.Entities.Structs;
+using Analytics.Domain.ValueObjects;
+using Shared.Domain.Aggregate.Helpers;
+using Shared.Domain.Aggregate.Interfaces;
+using Shared.Domain.Exceptions;
+using Shared.Domain.ValueObjects;
+
+namespace Analytics.Domain.Aggregates.Helpers;
+
+internal abstract class UpdateLevelHelper
+    : BaseHelper,
+        IHelper<UpdateLevelDomainRequest, UpdateLevelDomainResponse>
+{
+    public static UpdateLevelDomainResponse Execute(UpdateLevelDomainRequest data)
+    {
+        var @struct = GetLevelStruct(data);
+        var Level = new LevelEntity(@struct);
+        var response = new UpdateLevelDomainResponse { LevelId = Level.LevelId.Value };
+
+        if (data.Name != null)
+        {
+            var name = new NameValueObject(data.Name);
+            Level.UpdateName(name);
+            response.Name = Level.Name.Value;
+        }
+
+        if (data.Description != null)
+        {
+            var description = new DescriptionValueObject(data.Description);
+            Level.UpdateDescription(description);
+            response.Description = Level.Description?.Value;
+        }
+
+        if (data.Disable != null)
+        {
+            if (data.Disable == true)
+            {
+                Level.Disable();
+            }
+            else
+            {
+                Level.Enable();
+            }
+            response.Disabled = Level.Disabled.Value;
+        }
+
+        ValidateStructureFields(Level);
+        ValidateAmountDataToBeUpdated(response);
+
+        return response;
+    }
+
+    private static LevelStruct GetLevelStruct(UpdateLevelDomainRequest data)
+    {
+        var id = new LevelIdValueObject(data.LevelId);
+        return new LevelStruct { LevelId = id };
+    }
+
+    private static void ValidateAmountDataToBeUpdated(UpdateLevelDomainResponse response)
+    {
+        var count = response.GetType().GetProperties().Count(x => x.GetValue(response) != null);
+        if (count == 1)
+        {
+            throw new DomainException(
+                "No data to update",
+                [new ErrorValueObject("No fields to update", "No fields to update")]
+            );
+        }
+    }
+}
