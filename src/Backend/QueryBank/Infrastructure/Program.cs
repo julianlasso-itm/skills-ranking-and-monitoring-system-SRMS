7205b4c83c77f24e7fc98532da612744507927fb
Julian Lasso <julianlasso1133109@correo.itm.edu.co>
2024-05-19 09:54:16 -0500

    feat: Configure launch settings for EventConsumer and Infrastructure projects
    This commit adds and updates the launchSettings.json files for the EventConsumer and Infrastructure projects. The launchSettings.json file for the EventConsumer project includes settings for both HTTP and HTTPS protocols, with different application URLs and environment variables for each. The launchSettings.json file for the Infrastructure project includes settings for IIS Express and environment variables. These changes enhance the configuration of the projects for local development and testing.

--- src/Backend/QueryBank/Infrastructure/Program.cs
+++ src/Backend/QueryBank/Infrastructure/Program.cs
@@ -1,12 +1,68 @@
+using Microsoft.EntityFrameworkCore;
+using ProtoBuf.Grpc.Server;
+using QueryBank.Application.Repositories;
+using QueryBank.Infrastructure.AntiCorruption;
+using QueryBank.Infrastructure.AntiCorruption.Interfaces;
+using QueryBank.Infrastructure.Persistence;
+using QueryBank.Infrastructure.Persistence.Models;
+using QueryBank.Infrastructure.Persistence.Repositories;
+using QueryBank.Infrastructure.Services;
+using Shared.Infrastructure.Events;
+using Shared.Infrastructure.Interceptors;
+using Shared.Infrastructure.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddGrpc();
+// == Configure connection to the database ==
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+  options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnectionDataBase"));
+});
+// ==========================================
+
+// == Configure repositories ==
+builder.Services.AddScoped<ISkillRepository<SkillModel>, SkillRepository>();
+// ============================
+
+// == Configure dependency injection for services ==
+builder.Services.AddScoped<SharedEventHandler>();
+builder.Services.AddScoped<ApplicationService>();
+builder.Services.AddScoped<IAntiCorruptionLayer, AntiCorruptionLayer>();
+builder.Services.AddScoped<AntiCorruptionLayerService<AntiCorruptionLayer>>();
+// =================================================
+
+// == Configure interceptors for gRPC services ==
+builder.Services.AddSingleton<ErrorHandlingInterceptor>();
+// ==============================================
+
+// == Configure gRPC services ==
+builder.Services.AddCodeFirstGrpc(options =>
+{
+  options.Interceptors.Add<ErrorHandlingInterceptor>();
+});
+// ========================================
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-// app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+// == Ensure the database is created or migrated before starting the application ==
+using (var scope = app.Services.CreateScope())
+{
+  var services = scope.ServiceProvider;
+  var context = services.GetRequiredService<ApplicationDbContext>();
+  context.Database.Migrate();
+}
+// ================================================================================
+
+// == Configure gRPC services ==
+// app.MapGrpcService<QueryBankService>();
+// =============================
+
+app.MapGet(
+  "/",
+  () =>
+    "Communication with gRPC endpoints must be made through a gRPC client. "
+    + "To learn how to create a client, visit: "
+    + "https://go.microsoft.com/fwlink/?linkid=2086909"
+);
 
 app.Run();
