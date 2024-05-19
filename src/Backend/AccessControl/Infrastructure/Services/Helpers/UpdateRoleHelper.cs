5b788c7f1d3d77b9481cc8d3964e18dff46b4804
SebastianCamargo-ITM <juancamargo1135122@correo.itm.edu.co>
2024-05-19 09:59:16 -0500

    feat(backend): :sparkles: Services are modified and format is corrected

--- src/Backend/AccessControl/Infrastructure/Services/Helpers/UpdateRoleHelper.cs
+++ src/Backend/AccessControl/Infrastructure/Services/Helpers/UpdateRoleHelper.cs
@@ -1,43 +1,18 @@
-using AccessControl.Application.Commands;
-using AccessControl.Application.Responses;
 using AccessControl.Infrastructure.Services.Helpers.Base;
 using Shared.Infrastructure.ProtocolBuffers.AccessControl.Requests;
 using Shared.Infrastructure.ProtocolBuffers.AccessControl.Responses;
 
-namespace AccessControl.Infrastructure.Services.Helpers;
-
-internal class UpdateRoleHelper : BaseHelperServiceInfrastructure
+namespace AccessControl.Infrastructure.Services.Helpers
 {
-    public static async Task<UpdateRoleSecurityResponse> UpdateRoleAsync(
-        UpdateRoleSecurityRequest request
+  internal class UpdateRoleHelper : BaseHelperServiceInfrastructure
+  {
+    public static async Task<UpdateRoleAccessControlResponse> UpdateRoleAsync(
+      UpdateRoleAccessControlRequest request
     )
     {
-        var updateRoleCommand = MapToUpdateRoleCommand(request);
-        var data = await Application.UpdateRole(updateRoleCommand);
-        return MapToUpdateRoleResponse(data);
+      var command = AclInputMapper.ToUpdateRoleCommand(request);
+      var data = await Application.UpdateRole(command);
+      return AclOutputMapper.ToUpdateRoleResponse(data);
     }
-
-    private static UpdateRoleCommand MapToUpdateRoleCommand(UpdateRoleSecurityRequest request)
-    {
-        return new UpdateRoleCommand
-        {
-            RoleId = request.RoleId!,
-            Name = request.Name,
-            Description = request.Description,
-            Disable = request.Disable
-        };
-    }
-
-    private static UpdateRoleSecurityResponse MapToUpdateRoleResponse(
-        UpdateRoleApplicationResponse data
-    )
-    {
-        return new UpdateRoleSecurityResponse
-        {
-            RoleId = data.RoleId,
-            Name = data.Name,
-            Description = data.Description,
-            Disabled = data.Disabled,
-        };
-    }
+  }
 }
