0ec69766b289df9a00b8a67c3cac4c7f9b799731
SebastianCamargo-ITM <juancamargo1135122@correo.itm.edu.co>
2024-05-19 10:03:30 -0500

    perf(backend): :sparkles: Injections and dependencies are added

--- src/Backend/AccessControl/Infrastructure/Program.cs
+++ src/Backend/AccessControl/Infrastructure/Program.cs
@@ -1,33 +1,41 @@
 using AccessControl.Application.Repositories;
+using AccessControl.Infrastructure.AntiCorruption;
+using AccessControl.Infrastructure.AntiCorruption.Interfaces;
 using AccessControl.Infrastructure.Persistence;
 using AccessControl.Infrastructure.Persistence.Models;
 using AccessControl.Infrastructure.Persistence.Repositories;
 using AccessControl.Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 using ProtoBuf.Grpc.Server;
+using Shared.Application.Interfaces;
 using Shared.Infrastructure.Events;
 using Shared.Infrastructure.Interceptors;
+using Shared.Infrastructure.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // == Configure connection to the database ==
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+  options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnectionDataBase"));
 });
 // ==========================================
 
 // == Configure repositories ==
-builder.Services.AddScoped<IRoleRepository<RoleModel>, RoleRepository>(serviceProvider =>
-{
-    var dbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
-    return new RoleRepository(dbContext);
-});
+builder.Services.AddScoped<IUserRepository<UserModel>, UserRepository>();
+builder.Services.AddScoped<IRoleRepository<RoleModel>, RoleRepository>();
+// builder.Services.AddScoped<IUserPerRoleRepository<UserPerRoleModel>, UserPerRoleRepository>();
 // ============================
 
 // == Configure dependency injection for services ==
-builder.Services.AddScoped<SharedEventHandler>();
+builder.Services.AddScoped<MessageService>();
 builder.Services.AddScoped<ApplicationService>();
+builder.Services.AddScoped<SharedEventHandler>();
+builder.Services.AddScoped<ICacheService, CacheService>();
+builder.Services.AddScoped<IStoreService, StoreService>();
+builder.Services.AddScoped<IAntiCorruptionLayer, AntiCorruptionLayer>();
+builder.Services.AddScoped<AntiCorruptionLayerService<AntiCorruptionLayer>>();
+builder.Services.AddScoped<IEnvironment, EnvironmentService>();
 // =================================================
 
 // == Configure interceptors for gRPC services ==
@@ -37,7 +45,7 @@ builder.Services.AddSingleton<ErrorHandlingInterceptor>();
 // == Configure gRPC services ==
 builder.Services.AddCodeFirstGrpc(options =>
 {
-    options.Interceptors.Add<ErrorHandlingInterceptor>();
+  options.Interceptors.Add<ErrorHandlingInterceptor>();
 });
 // ========================================
 
@@ -46,9 +54,9 @@ var app = builder.Build();
 // == Ensure the database is created or migrated before starting the application ==
 using (var scope = app.Services.CreateScope())
 {
-    var services = scope.ServiceProvider;
-    var context = services.GetRequiredService<ApplicationDbContext>();
-    context.Database.Migrate();
+  var services = scope.ServiceProvider;
+  var context = services.GetRequiredService<ApplicationDbContext>();
+  context.Database.Migrate();
 }
 // ================================================================================
 
@@ -57,11 +65,11 @@ app.MapGrpcService<AccessControlService>();
 // =============================
 
 app.MapGet(
-    "/",
-    () =>
-        "Communication with gRPC endpoints must be made through a gRPC client. "
-        + "To learn how to create a client, "
-        + "visit: https://go.microsoft.com/fwlink/?linkid=2086909"
+  "/",
+  () =>
+    "Communication with gRPC endpoints must be made through a gRPC client. "
+    + "To learn how to create a client, "
+    + "visit: https://go.microsoft.com/fwlink/?linkid=2086909"
 );
 
 app.Run();
