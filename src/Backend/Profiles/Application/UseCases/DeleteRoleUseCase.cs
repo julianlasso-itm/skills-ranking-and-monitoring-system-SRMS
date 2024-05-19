58f66edfe951f81381d9ecb7933798b8caefc7f2
SebastianCamargo-ITM <juancamargo1135122@correo.itm.edu.co>
2024-05-19 10:15:16 -0500

    feat: Add use cases for deleting, registering, and updating levels

--- src/Backend/Profiles/Application/UseCases/DeleteRoleUseCase.cs
+++ src/Backend/Profiles/Application/UseCases/DeleteRoleUseCase.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json;
+using Profiles.Application.AntiCorruption.Interfaces;
 using Profiles.Application.Commands;
 using Profiles.Application.Repositories;
 using Profiles.Application.Responses;
@@ -8,46 +9,55 @@ using Profiles.Domain.Aggregates.Dto.Responses;
 using Profiles.Domain.Aggregates.Interfaces;
 using Shared.Application.Base;
 
-namespace Profiles.Application.UseCases;
-
-public sealed class DeleteRoleUseCase<TEntity>
-    : BaseUseCase<DeleteRoleCommand, DeleteRoleApplicationResponse, IPersonnelAggregateRoot>
-    where TEntity : class
+namespace Profiles.Application.UseCases
 {
+  public sealed class DeleteRoleUseCase<TEntity>
+    : BaseUseCase<
+      DeleteRoleCommand,
+      DeleteRoleApplicationResponse,
+      IPersonnelAggregateRoot,
+      IApplicationToDomain,
+      IDomainToApplication
+    >
+    where TEntity : class
+  {
     private readonly IRoleRepository<TEntity> _roleRepository;
     private const string Channel = $"{EventsConst.Prefix}.{EventsConst.EventRoleDeleted}";
 
     public DeleteRoleUseCase(
-        IPersonnelAggregateRoot aggregateRoot,
-        IRoleRepository<TEntity> roleRepository
+      IPersonnelAggregateRoot aggregateRoot,
+      IRoleRepository<TEntity> roleRepository,
+      IApplicationToDomain applicationToDomain,
+      IDomainToApplication domainToApplication
     )
-        : base(aggregateRoot)
+      : base(aggregateRoot, applicationToDomain, domainToApplication)
     {
-        _roleRepository = roleRepository;
+      _roleRepository = roleRepository;
     }
 
     public override async Task<DeleteRoleApplicationResponse> Handle(DeleteRoleCommand request)
     {
-        var dataDeleteRole = MapToRequestForDomain(request);
-        var role = AggregateRoot.DeleteRole(dataDeleteRole);
-        var response = MapToResponse(role);
-        _ = await Persistence(response);
-        EmitEvent(Channel, JsonSerializer.Serialize(response));
-        return response;
+      var dataDeleteRole = MapToRequestForDomain(request);
+      var role = AggregateRoot.DeleteRole(dataDeleteRole);
+      var response = MapToResponse(role);
+      _ = await Persistence(response);
+      EmitEvent(Channel, JsonSerializer.Serialize(response));
+      return response;
     }
 
     private DeleteRoleDomainRequest MapToRequestForDomain(DeleteRoleCommand request)
     {
-        return new DeleteRoleDomainRequest { RoleId = request.RoleId };
+      return new DeleteRoleDomainRequest { RoleId = request.RoleId };
     }
 
     private DeleteRoleApplicationResponse MapToResponse(DeleteRoleDomainResponse role)
     {
-        return new DeleteRoleApplicationResponse { RoleId = role.RoleId };
+      return new DeleteRoleApplicationResponse { RoleId = role.RoleId };
     }
 
     private async Task<TEntity> Persistence(DeleteRoleApplicationResponse response)
     {
-        return await _roleRepository.DeleteAsync(Guid.Parse(response.RoleId));
+      return await _roleRepository.DeleteAsync(Guid.Parse(response.RoleId));
     }
+  }
 }
--- src/Backend/Profiles/Application/UseCases/UpdateRoleUseCase.cs
+++ src/Backend/Profiles/Application/UseCases/UpdateRoleUseCase.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json;
+using Profiles.Application.AntiCorruption.Interfaces;
 using Profiles.Application.Commands;
 using Profiles.Application.Repositories;
 using Profiles.Application.Responses;
@@ -8,58 +9,67 @@ using Profiles.Domain.Aggregates.Dto.Responses;
 using Profiles.Domain.Aggregates.Interfaces;
 using Shared.Application.Base;
 
-namespace Profiles.Application.UseCases;
-
-public sealed class UpdateRoleUseCase<TEntity>
-    : BaseUseCase<UpdateRoleCommand, UpdateRoleApplicationResponse, IPersonnelAggregateRoot>
-    where TEntity : class
+namespace Profiles.Application.UseCases
 {
+  public sealed class UpdateRoleUseCase<TEntity>
+    : BaseUseCase<
+      UpdateRoleCommand,
+      UpdateRoleApplicationResponse,
+      IPersonnelAggregateRoot,
+      IApplicationToDomain,
+      IDomainToApplication
+    >
+    where TEntity : class
+  {
     private readonly IRoleRepository<TEntity> _roleRepository;
     private const string Channel = $"{EventsConst.Prefix}.{EventsConst.EventRoleUpdated}";
 
     public UpdateRoleUseCase(
-        IPersonnelAggregateRoot aggregateRoot,
-        IRoleRepository<TEntity> roleRepository
+      IPersonnelAggregateRoot aggregateRoot,
+      IRoleRepository<TEntity> roleRepository,
+      IApplicationToDomain applicationToDomain,
+      IDomainToApplication domainToApplication
     )
-        : base(aggregateRoot)
+      : base(aggregateRoot, applicationToDomain, domainToApplication)
     {
-        _roleRepository = roleRepository;
+      _roleRepository = roleRepository;
     }
 
     public override async Task<UpdateRoleApplicationResponse> Handle(UpdateRoleCommand request)
     {
-        var dataUpdateRole = MapToRequestForDomain(request);
-        var role = AggregateRoot.UpdateRole(dataUpdateRole);
-        var response = MapToResponse(role);
-        _ = await Persistence(response);
-        EmitEvent(Channel, JsonSerializer.Serialize(response));
-        return response;
+      var dataUpdateRole = MapToRequestForDomain(request);
+      var role = AggregateRoot.UpdateRole(dataUpdateRole);
+      var response = MapToResponse(role);
+      _ = await Persistence(response);
+      EmitEvent(Channel, JsonSerializer.Serialize(response));
+      return response;
     }
 
     private UpdateRoleDomainRequest MapToRequestForDomain(UpdateRoleCommand request)
     {
-        return new UpdateRoleDomainRequest
-        {
-            RoleId = request.RoleId,
-            Name = request.Name,
-            Description = request.Description,
-            Disable = request.Disable,
-        };
+      return new UpdateRoleDomainRequest
+      {
+        RoleId = request.RoleId,
+        Name = request.Name,
+        Description = request.Description,
+        Disable = request.Disable,
+      };
     }
 
     private UpdateRoleApplicationResponse MapToResponse(UpdateRoleDomainResponse role)
     {
-        return new UpdateRoleApplicationResponse
-        {
-            RoleId = role.RoleId,
-            Name = role.Name,
-            Description = role.Description,
-            Disabled = role.Disabled,
-        };
+      return new UpdateRoleApplicationResponse
+      {
+        RoleId = role.RoleId,
+        Name = role.Name,
+        Description = role.Description,
+        Disabled = role.Disabled,
+      };
     }
 
     private async Task<TEntity> Persistence(UpdateRoleApplicationResponse response)
     {
-        return await _roleRepository.UpdateAsync(Guid.Parse(response.RoleId), response);
+      return await _roleRepository.UpdateAsync(Guid.Parse(response.RoleId), response);
     }
+  }
 }
