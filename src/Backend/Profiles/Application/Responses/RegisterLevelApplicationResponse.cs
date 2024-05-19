17cc1e445e9e6d22e469187435a79e103066c624
SebastianCamargo-ITM <juancamargo1135122@correo.itm.edu.co>
2024-05-19 10:14:39 -0500

    refactor(backend): :art: Improving response and repository structure

--- src/Backend/Profiles/Application/Responses/RegisterLevelApplicationResponse.cs
+++ src/Backend/Profiles/Application/Responses/RegisterLevelApplicationResponse.cs
@@ -1,8 +1,8 @@
-﻿namespace Profiles.Application.Responses
+namespace Profiles.Application.Responses
 {
-  public sealed class RegisterRoleApplicationResponse
+  public sealed class RegisterLevelApplicationResponse
   {
-    public required string RoleId { get; init; }
+    public required string LevelId { get; init; }
     public required string Name { get; init; }
     public string? Description { get; init; }
     public required bool Disabled { get; init; }
