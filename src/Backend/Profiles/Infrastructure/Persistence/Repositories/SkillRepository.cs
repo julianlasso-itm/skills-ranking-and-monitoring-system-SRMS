18da90ed4fb9d88511526eed3573a9a6985d5eb8
Julian Lasso <julianlasso1133109@correo.itm.edu.co>
2024-05-19 09:56:05 -0500

    feat: Refactor RoleModel, ProvinceModel, and SkillModel classes
    This commit refactors the RoleModel, ProvinceModel, and SkillModel classes in the Profiles.Infrastructure.Persistence.Models namespace. The changes include adding missing namespaces, fixing indentation, and improving code structure. This refactor enhances the readability and maintainability of the codebase.

--- src/Backend/Profiles/Infrastructure/Persistence/Repositories/SkillRepository.cs
+++ src/Backend/Profiles/Infrastructure/Persistence/Repositories/SkillRepository.cs
@@ -1,39 +1,38 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Profiles.Application.Repositories;
+﻿using Profiles.Application.Repositories;
 using Profiles.Application.Responses;
 using Profiles.Infrastructure.Persistence.Models;
 using Shared.Infrastructure.Persistence.Repositories;
 
 namespace Profiles.Infrastructure.Persistence.Repositories
 {
-    public class SkillRepository : BaseRepository<SkillModel>, ISkillRepository<SkillModel>
+  public class SkillRepository : BaseRepository<SkillModel>, ISkillRepository<SkillModel>
+  {
+    public SkillRepository(ApplicationDbContext context)
+      : base(context) { }
+
+    public Task<SkillModel> AddAsync(RegisterSkillApplicationResponse entity)
     {
-        public SkillRepository(DbContext context)
-            : base(context) { }
-
-        public Task<SkillModel> AddAsync(RegisterSkillApplicationResponse entity)
-        {
-            var skill = new SkillModel
-            {
-                SkillId = Guid.Parse(entity.SkillId),
-                Name = entity.Name,
-                Disabled = entity.Disabled,
-            };
-            return AddAsync(skill);
-        }
-
-        public Task<SkillModel> UpdateAsync(Guid id, UpdateSkillApplicationResponse entity)
-        {
-            var skill = new SkillModel { SkillId = id };
-            if (entity.Name != null)
-            {
-                skill.Name = entity.Name;
-            }
-            if (entity.Disabled != null)
-            {
-                skill.Disabled = (bool)entity.Disabled;
-            }
-            return UpdateAsync(id, skill);
-        }
+      var skill = new SkillModel
+      {
+        SkillId = Guid.Parse(entity.SkillId),
+        Name = entity.Name,
+        Disabled = entity.Disabled,
+      };
+      return AddAsync(skill);
     }
+
+    public Task<SkillModel> UpdateAsync(Guid id, UpdateSkillApplicationResponse entity)
+    {
+      var skill = new SkillModel { SkillId = id };
+      if (entity.Name != null)
+      {
+        skill.Name = entity.Name;
+      }
+      if (entity.Disabled != null)
+      {
+        skill.Disabled = (bool)entity.Disabled;
+      }
+      return UpdateAsync(id, skill);
+    }
+  }
 }
