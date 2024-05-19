e83b1daea0a6b9442a6fbbb1a2b4e0a88b347271
SebastianCamargo-ITM <juancamargo1135122@correo.itm.edu.co>
2024-05-19 10:02:38 -0500

    feat(backend): :sparkles: This commit adds the `UserPerRoleModel` DbSet to the `ApplicationDbContext` class in the `AccessControl.Infrastructure.Persistence` namespace. This allows for database operations on the `UserPerRole` entity.

--- src/Backend/AccessControl/Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/Backend/AccessControl/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,17 +1,20 @@
 using AccessControl.Infrastructure.Persistence.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace AccessControl.Infrastructure.Persistence;
-
-public class ApplicationDbContext : DbContext
+namespace AccessControl.Infrastructure.Persistence
 {
+  public class ApplicationDbContext : DbContext
+  {
+    public DbSet<UserModel> User { get; set; }
     public DbSet<RoleModel> Role { get; set; }
+    public DbSet<UserPerRoleModel> UserPerRole { get; set; }
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options) { }
+      : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        Seeds.SeedRoles(modelBuilder);
+      Seeds.SeedRoles(modelBuilder);
     }
+  }
 }
