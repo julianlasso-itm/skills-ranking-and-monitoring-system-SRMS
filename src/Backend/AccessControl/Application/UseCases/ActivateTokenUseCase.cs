c19d8cb3d4f8c6aa2cefcfbfdb631e26ece0ec29
SebastianCamargo-ITM <juancamargo1135122@correo.itm.edu.co>
2024-06-09 12:18:09 -0500

    feat: :sparkles: application layer is added for access control

---
+++ src/Backend/AccessControl/Application/UseCases/ActivateTokenUseCase.cs
@@ -0,0 +1,80 @@
+using System.Net;
+using System.Text.Json;
+using AccessControl.Application.AntiCorruption.Interfaces;
+using AccessControl.Application.Commands;
+using AccessControl.Application.Repositories;
+using AccessControl.Application.Responses;
+using AccessControl.Domain.Aggregates.Constants;
+using AccessControl.Domain.Aggregates.Interfaces;
+using Shared.Application.Base;
+using Shared.Application.Interfaces;
+using ApplicationException = Shared.Application.Exceptions.ApplicationException;
+
+namespace AccessControl.Application.UseCases
+{
+    public sealed class ActivateTokenUseCase<TEntity>
+        : BaseUseCase<
+            ActivateTokenCommand,
+            ActivationTokenApplicationResponse,
+            ISecurityAggregateRoot,
+            IApplicationToDomain,
+            IDomainToApplication
+        >
+        where TEntity : class
+    {
+        private const string Channel =
+            $"{EventsConst.Prefix}.{EventsConst.EventCredentialActivated}";
+        private readonly IUserRepository<TEntity> _userRepository;
+        private readonly ICacheService _cacheService;
+
+        public ActivateTokenUseCase(
+            IUserRepository<TEntity> userRepository,
+            ICacheService cacheService,
+            ISecurityAggregateRoot aggregateRoot,
+            IApplicationToDomain applicationToDomain,
+            IDomainToApplication domainToApplication
+        )
+            : base(aggregateRoot, applicationToDomain, domainToApplication)
+        {
+            _userRepository = userRepository;
+            _cacheService = cacheService;
+        }
+
+        public override async Task<ActivationTokenApplicationResponse> Handle(
+            ActivateTokenCommand request
+        )
+        {
+            var command = AclInputMapper.ToActivateTokenDomainRequest(request);
+            var response = AggregateRoot.ValidateActivationToken(command);
+            var userId = GetUserIdFromCache(response.ActivationToken);
+            var requestForDomain = AclInputMapper.ToActiveCredentialDomainRequest(
+                new ActiveUserCommand { UserId = userId }
+            );
+            var user = AggregateRoot.ActiveCredential(requestForDomain);
+            await ChangeUserStatusInDatabase(userId);
+            RemoveTokenFromCache(response.ActivationToken);
+            EmitEvent(Channel, JsonSerializer.Serialize(user));
+            return AclOutputMapper.ToActivationTokenApplicationResponse(response);
+        }
+
+        private string GetUserIdFromCache(string token)
+        {
+            return _cacheService.Get($"token:{token}")
+                ?? throw new ApplicationException(
+                    "Token not found in cache",
+                    HttpStatusCode.NotFound
+                );
+        }
+
+        private async Task ChangeUserStatusInDatabase(string userId)
+        {
+            var data = new UpdateUserApplicationResponse { UserId = userId, Disabled = false };
+            await _userRepository.UpdateAsync(Guid.Parse(userId), data);
+        }
+
+        private void RemoveTokenFromCache(string token)
+        {
+            _cacheService.Remove($"token:{token}");
+        }
+    }
+}
---
+++ src/Backend/AccessControl/Application/UseCases/RegisterRoleUseCase.cs
@@ -0,0 +1,65 @@
+using System.Text.Json;
+using AccessControl.Application.AntiCorruption.Interfaces;
+using AccessControl.Application.Commands;
+using AccessControl.Application.Repositories;
+using AccessControl.Application.Responses;
+using AccessControl.Domain.Aggregates.Constants;
+using AccessControl.Domain.Aggregates.Dto.Responses;
+using AccessControl.Domain.Aggregates.Interfaces;
+using Shared.Application.Base;
+
+namespace AccessControl.Application.UseCases
+{
+    public sealed class RegisterRoleUseCase<TEntity>
+        : BaseUseCase<
+            RegisterRoleCommand,
+            RegisterRoleApplicationResponse,
+            ISecurityAggregateRoot,
+            IApplicationToDomain,
+            IDomainToApplication
+        >
+        where TEntity : class
+    {
+        private readonly IRoleRepository<TEntity> _roleRepository;
+        private const string Channel = $"{EventsConst.Prefix}.{EventsConst.EventRoleRegistered}";
+
+        public RegisterRoleUseCase(
+            ISecurityAggregateRoot aggregateRoot,
+            IRoleRepository<TEntity> roleRepository,
+            IApplicationToDomain applicationToDomain,
+            IDomainToApplication domainToApplication
+        )
+            : base(aggregateRoot, applicationToDomain, domainToApplication)
+        {
+            _roleRepository = roleRepository;
+        }
+
+        public override async Task<RegisterRoleApplicationResponse> Handle(
+            RegisterRoleCommand request
+        )
+        {
+            var newRole = AclInputMapper.ToRegisterRoleDomainRequest(request);
+            var role = AggregateRoot.RegisterRole(newRole);
+            var response = MapToResponse(role);
+            _ = await Persistence(response);
+            EmitEvent(Channel, JsonSerializer.Serialize(response));
+            return response;
+        }
+
+        private RegisterRoleApplicationResponse MapToResponse(RegisterRoleDomainResponse role)
+        {
+            return new RegisterRoleApplicationResponse
+            {
+                RoleId = role.RoleId,
+                Name = role.Name,
+                Description = role.Description,
+                Disabled = role.Disabled,
+            };
+        }
+
+        private async Task<TEntity> Persistence(RegisterRoleApplicationResponse response)
+        {
+            return await _roleRepository.AddAsync(response);
+        }
+    }
+}
