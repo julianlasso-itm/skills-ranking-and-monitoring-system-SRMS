d4f0fd6daa44ad52b0b75d16d64556f03a2800e2
SebastianCamargo-ITM <juancamargo1135122@correo.itm.edu.co>
2024-06-09 12:22:58 -0500

    feat: Add VerifyTokeDto and UpdateUserRequestDto classes
    The code changes include adding two new classes, VerifyTokeDto and UpdateUserRequestDto, to the ApiGateway.Infrastructure.Dto namespace. These classes are used for handling token verification and updating user information.

---
+++ src/Backend/ApiGateway/Infrastructure/Controllers/Base/BaseController.cs
@@ -0,0 +1,77 @@
+using System.ComponentModel;
+using System.Net;
+using Grpc.Core;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Application.Interfaces;
+using Shared.Infrastructure.Exceptions;
+
+namespace ApiGateway.Infrastructure.Controllers.Base
+{
+    public abstract class BaseController : Controller
+    {
+        const string ContentType = "application/json";
+        protected readonly ICacheService CacheService;
+
+        public BaseController(ICacheService cacheService)
+        {
+            CacheService = cacheService;
+        }
+
+        [NonAction]
+        protected async Task<IActionResult> HandleAsync(Func<Task<IActionResult>> action)
+        {
+            try
+            {
+                return await action();
+            }
+            catch (RpcException exception)
+            {
+                return HandleRpcException(exception);
+            }
+            catch (Exception exception)
+            {
+                return HandleException(exception);
+            }
+        }
+
+        [NonAction]
+        protected void DebugRequest<T>(T request)
+        {
+            if (request != null)
+            {
+                Console.WriteLine("==================================");
+                Console.WriteLine("Request:");
+                foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(request))
+                {
+                    var name = descriptor.Name;
+                    var value = descriptor.GetValue(request);
+                    Console.WriteLine("{0}={1}", name, value);
+                }
+                Console.WriteLine("==================================");
+            }
+        }
+
+        private ContentResult HandleRpcException(RpcException exception)
+        {
+            return HandleExceptionResponse(
+                exception.Status.Detail,
+                (int)StatusCodeConverter.GetHttpStatusCodeFromGrpcStatus(exception.StatusCode)
+            );
+        }
+
+        private ContentResult HandleException(Exception exception)
+        {
+            return HandleExceptionResponse(
+                exception.Message,
+                (int)HttpStatusCode.InternalServerError
+            );
+        }
+
+        private ContentResult HandleExceptionResponse(string message, int statusCode)
+        {
+            var content = Content(message, ContentType);
+            content.StatusCode = statusCode;
+            return content;
+        }
+    }
+}
---
+++ src/Backend/ApiGateway/Infrastructure/Services/AnalyticsService.cs
@@ -0,0 +1,51 @@
+using ApiGateway.Infrastructure.Services.Base;
+using ProtoBuf.Grpc;
+using Shared.Infrastructure.ProtocolBuffers.Analytics;
+using Shared.Infrastructure.ProtocolBuffers.Analytics.Requests;
+using Shared.Infrastructure.ProtocolBuffers.Analytics.Responses;
+
+namespace ApiGateway.Infrastructure.Services
+{
+    public class AnalyticsService : BaseServices<IAnalyticsServices>, IAnalyticsServices
+    {
+        const string UrlMicroservice = "http://localhost:5098";
+
+        public AnalyticsService(HttpClientHandler? httpClientHandler = null)
+            : base(httpClientHandler)
+        {
+            CreateChannel(UrlMicroservice);
+        }
+
+        public Task<RegisterLevelAnalyticsResponse> RegisterLevelAsync(
+            RegisterLevelAnalyticsRequest request,
+            CallContext context = default
+        )
+        {
+            return Client.RegisterLevelAsync(request, context);
+        }
+
+        public Task<UpdateLevelAnalyticsResponse> UpdateLevelAsync(
+            UpdateLevelAnalyticsRequest request,
+            CallContext context = default
+        )
+        {
+            return Client.UpdateLevelAsync(request, context);
+        }
+
+        public Task<DeleteLevelAnalyticsResponse> DeleteLevelAsync(
+            DeleteLevelAnalyticsRequest request,
+            CallContext context = default
+        )
+        {
+            return Client.DeleteLevelAsync(request, context);
+        }
+
+        public Task<GetLevelsAnalyticsResponse> GetLevelsAsync(
+            GetLevelsAnalyticsRequest request,
+            CallContext context = default
+        )
+        {
+            return Client.GetLevelsAsync(request, context);
+        }
+    }
+}
