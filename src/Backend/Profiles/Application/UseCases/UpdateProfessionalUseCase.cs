ed664e6cf04051c77ef980b1a46a7a7be85ab12c
SebastianCamargo-ITM <juancamargo1135122@correo.itm.edu.co>
2024-06-09 12:23:48 -0500

    feat(Backend): :sparkles: Add application layer fot assesments

---
+++ src/Backend/Profiles/Application/UseCases/UpdateProfessionalUseCase.cs
@@ -0,0 +1,85 @@
+﻿using System.Text.Json;
+using Profiles.Application.AntiCorruption.Interfaces;
+using Profiles.Application.Commands;
+using Profiles.Application.Repositories;
+using Profiles.Application.Responses;
+using Profiles.Domain.Aggregates.Constants;
+using Profiles.Domain.Aggregates.Dto.Requests;
+using Profiles.Domain.Aggregates.Interfaces;
+using Shared.Application.Base;
+
+namespace Profiles.Application.UseCases
+{
+    public class UpdateProfessionalUseCase<TEntity>
+        : BaseUseCase<
+            UpdateProfessionalCommand,
+            UpdateProfessionalApplicationResponse,
+            IPersonnelAggregateRoot,
+            IApplicationToDomain,
+            IDomainToApplication
+        >
+        where TEntity : class
+    {
+        private IProfessionalRepository<TEntity> _professionalRepository;
+
+        private const string Channel =
+            $"{EventsConst.Prefix}.{EventsConst.EventProfessionalUpdated}";
+
+        public UpdateProfessionalUseCase(
+            IPersonnelAggregateRoot aggregateRoot,
+            IProfessionalRepository<TEntity> professionalRepository,
+            IApplicationToDomain applicationToDomain,
+            IDomainToApplication domainToApplication
+        )
+            : base(aggregateRoot, applicationToDomain, domainToApplication)
+        {
+            _professionalRepository = professionalRepository;
+        }
+
+        public override async Task<UpdateProfessionalApplicationResponse> Handle(
+            UpdateProfessionalCommand request
+        )
+        {
+            var dataUpdateProfessional = MapToRequestForDomain(request);
+            var professional = AggregateRoot.UpdateProfessional(dataUpdateProfessional);
+            var response = MapToResponse(professional);
+            _ = await Persistence(response);
+            EmitEvent(Channel, JsonSerializer.Serialize(response));
+            return response;
+        }
+
+        private UpdateProfessionalDomainRequest MapToRequestForDomain(
+            UpdateProfessionalCommand request
+        )
+        {
+            return new UpdateProfessionalDomainRequest
+            {
+                ProfessionalId = request.ProfessionalId,
+                Name = request.Name,
+                Email = request.Email,
+                Disable = request.Disable,
+            };
+        }
+
+        private UpdateProfessionalApplicationResponse MapToResponse(
+            UpdateProfessionalDomainResponse professional
+        )
+        {
+            return new UpdateProfessionalApplicationResponse
+            {
+                ProfessionalId = professional.ProfessionalId,
+                Name = professional.Name,
+                Email = professional.Email,
+                Disabled = professional.Disabled,
+            };
+        }
+
+        private async Task<TEntity> Persistence(UpdateProfessionalApplicationResponse response)
+        {
+            return await _professionalRepository.UpdateAsync(
+                Guid.Parse(response.ProfessionalId),
+                response
+            );
+        }
+    }
+}
---
+++ src/Backend/Profiles/Application/UseCases/UpdateRoleUseCase.cs
@@ -0,0 +1,77 @@
+﻿using System.Text.Json;
+using Profiles.Application.AntiCorruption.Interfaces;
+using Profiles.Application.Commands;
+using Profiles.Application.Repositories;
+using Profiles.Application.Responses;
+using Profiles.Domain.Aggregates.Constants;
+using Profiles.Domain.Aggregates.Dto.Requests;
+using Profiles.Domain.Aggregates.Dto.Responses;
+using Profiles.Domain.Aggregates.Interfaces;
+using Shared.Application.Base;
+
+namespace Profiles.Application.UseCases
+{
+    public sealed class UpdateRoleUseCase<TEntity>
+        : BaseUseCase<
+            UpdateRoleCommand,
+            UpdateRoleApplicationResponse,
+            IPersonnelAggregateRoot,
+            IApplicationToDomain,
+            IDomainToApplication
+        >
+        where TEntity : class
+    {
+        private readonly IRoleRepository<TEntity> _roleRepository;
+        private const string Channel = $"{EventsConst.Prefix}.{EventsConst.EventRoleUpdated}";
+
+        public UpdateRoleUseCase(
+            IPersonnelAggregateRoot aggregateRoot,
+            IRoleRepository<TEntity> roleRepository,
+            IApplicationToDomain applicationToDomain,
+            IDomainToApplication domainToApplication
+        )
+            : base(aggregateRoot, applicationToDomain, domainToApplication)
+        {
+            _roleRepository = roleRepository;
+        }
+
+        public override async Task<UpdateRoleApplicationResponse> Handle(UpdateRoleCommand request)
+        {
+            var dataUpdateRole = MapToRequestForDomain(request);
+            var role = AggregateRoot.UpdateRole(dataUpdateRole);
+            var response = MapToResponse(role);
+            _ = await Persistence(response);
+            EmitEvent(Channel, JsonSerializer.Serialize(response));
+            return response;
+        }
+
+        private UpdateRoleDomainRequest MapToRequestForDomain(UpdateRoleCommand request)
+        {
+            return new UpdateRoleDomainRequest
+            {
+                RoleId = request.RoleId,
+                Name = request.Name,
+                Description = request.Description,
+                Disable = request.Disable,
+                Skills = request.Skills,
+            };
+        }
+
+        private UpdateRoleApplicationResponse MapToResponse(UpdateRoleDomainResponse role)
+        {
+            return new UpdateRoleApplicationResponse
+            {
+                RoleId = role.RoleId,
+                Name = role.Name,
+                Description = role.Description,
+                Disabled = role.Disabled,
+                Skills = role.Skills,
+            };
+        }
+
+        private async Task<TEntity> Persistence(UpdateRoleApplicationResponse response)
+        {
+            return await _roleRepository.UpdateAsync(Guid.Parse(response.RoleId), response);
+        }
+    }
+}
