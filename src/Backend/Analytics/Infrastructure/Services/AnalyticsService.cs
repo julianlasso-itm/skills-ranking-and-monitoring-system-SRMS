cbdda5390eb55f5903b2681a7a578d7fb7ab8389
SebastianCamargo-ITM <juancamargo1135122@correo.itm.edu.co>
2024-06-09 12:22:50 -0500

    chore: Add appsettings files for development environment

---
+++ src/Backend/Analytics/Infrastructure/Services/AnalyticsService.cs
@@ -0,0 +1,54 @@
+using Analytics.Infrastructure.Services.Helpers;
+using ProtoBuf.Grpc;
+using Shared.Infrastructure.ProtocolBuffers.Analytics;
+using Shared.Infrastructure.ProtocolBuffers.Analytics.Requests;
+using Shared.Infrastructure.ProtocolBuffers.Analytics.Responses;
+
+namespace Analytics.Infrastructure.Services
+{
+    public class AnalyticsService : IAnalyticsServices
+    {
+        private readonly ApplicationService _applicationService;
+
+        public AnalyticsService(ApplicationService applicationService)
+        {
+            _applicationService = applicationService;
+        }
+
+        public async Task<RegisterLevelAnalyticsResponse> RegisterLevelAsync(
+            RegisterLevelAnalyticsRequest request,
+            CallContext context = default
+        )
+        {
+            RegisterLevelHelper.SetApplication(_applicationService.GetApplication());
+            return await RegisterLevelHelper.RegisterLevelAsync(request);
+        }
+
+        public async Task<UpdateLevelAnalyticsResponse> UpdateLevelAsync(
+            UpdateLevelAnalyticsRequest request,
+            CallContext context = default
+        )
+        {
+            UpdateLevelHelper.SetApplication(_applicationService.GetApplication());
+            return await UpdateLevelHelper.UpdateLevelAsync(request);
+        }
+
+        public async Task<DeleteLevelAnalyticsResponse> DeleteLevelAsync(
+            DeleteLevelAnalyticsRequest request,
+            CallContext context = default
+        )
+        {
+            DeleteLevelHelper.SetApplication(_applicationService.GetApplication());
+            return await DeleteLevelHelper.DeleteLevelAsync(request);
+        }
+
+        public async Task<GetLevelsAnalyticsResponse> GetLevelsAsync(
+            GetLevelsAnalyticsRequest request,
+            CallContext context = default
+        )
+        {
+            GetLevelsHelper.SetApplication(_applicationService.GetApplication());
+            return await GetLevelsHelper.GetLevelsAsync(request);
+        }
+    }
+}
---
+++ src/Backend/Analytics/Infrastructure/Services/Helpers/GetLevelsHelper.cs
@@ -0,0 +1,53 @@
+using Analytics.Application.Commands;
+using Analytics.Application.Responses;
+using Analytics.Infrastructure.Persistence.Models;
+using Analytics.Infrastructure.Services.Helpers.Base;
+using Shared.Infrastructure.ProtocolBuffers.Analytics.Requests;
+using Shared.Infrastructure.ProtocolBuffers.Analytics.Responses;
+
+namespace Analytics.Infrastructure.Services.Helpers
+{
+    internal class GetLevelsHelper : BaseHelperServiceInfrastructure
+    {
+        public static async Task<GetLevelsAnalyticsResponse> GetLevelsAsync(
+            GetLevelsAnalyticsRequest request
+        )
+        {
+            var getLevelsCommand = MapToGetLevelsCommand(request);
+            var data = await Application.GetLevels(getLevelsCommand);
+            return MapToGetLevelsResponse(data);
+        }
+
+        private static GetLevelsCommand MapToGetLevelsCommand(GetLevelsAnalyticsRequest request)
+        {
+            return new GetLevelsCommand
+            {
+                Page = request.Page,
+                Limit = request.Limit,
+                Filter = request.Filter,
+                FilterBy = request.FilterBy,
+                Sort = request.Sort,
+                Order = request.Order
+            };
+        }
+
+        private static GetLevelsAnalyticsResponse MapToGetLevelsResponse(
+            GetLevelsApplicationResponse<LevelModel> data
+        )
+        {
+            return new GetLevelsAnalyticsResponse
+            {
+                Levels = data
+                    .Levels.Select(level => new LevelAnalytics
+                    {
+                        LevelId = level.LevelId.ToString(),
+                        Name = level.Name,
+                        Description = level.Description,
+                        Disabled = level.Disabled
+                    })
+                    .ToList(),
+                Total = data.Total
+            };
+        }
+    }
+}
