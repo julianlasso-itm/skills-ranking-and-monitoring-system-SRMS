88095863279ab3f17e6295b2f5cfb6425b29ed7b
SebastianCamargo-ITM <juancamargo1135122@correo.itm.edu.co>
2024-06-09 12:22:21 -0500

    feat: Add level domain DTOs and value objects

---
+++ src/Backend/Analytics/Domain/Aggregates/Helpers/DeleteLevelHelper.cs
@@ -0,0 +1,32 @@
+using Analytics.Domain.Aggregates.Dto.Requests;
+using Analytics.Domain.Aggregates.Dto.Responses;
+using Analytics.Domain.Entities.Records;
+using Analytics.Domain.ValueObjects;
+using Shared.Domain.Aggregate.Helpers;
+using Shared.Domain.Aggregate.Interfaces;
+
+namespace Analytics.Domain.Aggregates.Helpers
+{
+    internal class DeleteLevelHelper
+        : BaseHelper,
+            IHelper<DeleteLevelDomainRequest, DeleteLevelDomainResponse>
+    {
+        public static DeleteLevelDomainResponse Execute(DeleteLevelDomainRequest request)
+        {
+            var record = GetLevelRecord(request);
+            ValidateRecordFields(record);
+            return MapToResponse(record);
+        }
+
+        private static LevelRecords GetLevelRecord(DeleteLevelDomainRequest request)
+        {
+            var id = new LevelIdValueObject(request.LevelId);
+            return new LevelRecords { LevelId = id };
+        }
+
+        private static DeleteLevelDomainResponse MapToResponse(LevelRecords Level)
+        {
+            return new DeleteLevelDomainResponse { LevelId = Level.LevelId.Value };
+        }
+    }
+}
---
+++ src/Backend/Analytics/Domain/Aggregates/Helpers/UpdateLevelHelper.cs
@@ -0,0 +1,74 @@
+using Analytics.Domain.Aggregates.Dto.Requests;
+using Analytics.Domain.Aggregates.Dto.Responses;
+using Analytics.Domain.Entities;
+using Analytics.Domain.Entities.Records;
+using Analytics.Domain.ValueObjects;
+using Shared.Domain.Aggregate.Helpers;
+using Shared.Domain.Aggregate.Interfaces;
+using Shared.Domain.Exceptions;
+using Shared.Domain.ValueObjects;
+
+namespace Analytics.Domain.Aggregates.Helpers
+{
+    internal class UpdateLevelHelper
+        : BaseHelper,
+            IHelper<UpdateLevelDomainRequest, UpdateLevelDomainResponse>
+    {
+        public static UpdateLevelDomainResponse Execute(UpdateLevelDomainRequest data)
+        {
+            var record = GetLevelRecord(data);
+            var Level = new LevelEntity(record);
+            var response = new UpdateLevelDomainResponse { LevelId = Level.LevelId.Value };
+
+            if (data.Name != null)
+            {
+                var name = new NameValueObject(data.Name);
+                Level.UpdateName(name);
+                response.Name = Level.Name.Value;
+            }
+
+            if (data.Description != null)
+            {
+                var description = new DescriptionValueObject(data.Description);
+                Level.UpdateDescription(description);
+                response.Description = Level.Description?.Value;
+            }
+
+            if (data.Disable != null)
+            {
+                if (data.Disable == true)
+                {
+                    Level.Disable();
+                }
+                else
+                {
+                    Level.Enable();
+                }
+                response.Disabled = Level.Disabled.Value;
+            }
+
+            ValidateRecordFields(Level);
+            ValidateAmountDataToBeUpdated(response);
+
+            return response;
+        }
+
+        private static LevelRecords GetLevelRecord(UpdateLevelDomainRequest data)
+        {
+            var id = new LevelIdValueObject(data.LevelId);
+            return new LevelRecords { LevelId = id };
+        }
+
+        private static void ValidateAmountDataToBeUpdated(UpdateLevelDomainResponse response)
+        {
+            var count = response.GetType().GetProperties().Count(x => x.GetValue(response) != null);
+            if (count == 1)
+            {
+                throw new DomainException(
+                    "No data to update",
+                    [new ErrorValueObject("No fields to update", "No fields to update")]
+                );
+            }
+        }
+    }
+}
