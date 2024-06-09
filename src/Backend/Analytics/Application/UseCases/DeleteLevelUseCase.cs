915b2e21ba5409476f38517812f75306217213ff
SebastianCamargo-ITM <juancamargo1135122@correo.itm.edu.co>
2024-06-09 12:21:47 -0500

    feat(Backend): :sparkles: Add application level commands and responses for level management

---
+++ src/Backend/Analytics/Application/UseCases/DeleteLevelUseCase.cs
@@ -0,0 +1,65 @@
+using System.Text.Json;
+using Analytics.Application.AntiCorruption.Interfaces;
+using Analytics.Application.Commands;
+using Analytics.Application.Repositories;
+using Analytics.Application.Responses;
+using Analytics.Domain.Aggregates.Constants;
+using Analytics.Domain.Aggregates.Dto.Requests;
+using Analytics.Domain.Aggregates.Dto.Responses;
+using Analytics.Domain.Aggregates.Interfaces;
+using Shared.Application.Base;
+
+namespace Analytics.Application.UseCases
+{
+    public sealed class DeleteLevelUseCase<TEntity>
+        : BaseUseCase<
+            DeleteLevelCommand,
+            DeleteLevelApplicationResponse,
+            IAggregateRoot,
+            IApplicationToDomain,
+            IDomainToApplication
+        >
+        where TEntity : class
+    {
+        private readonly ILevelRepository<TEntity> _levelRepository;
+        private const string Channel = $"{EventsConst.Prefix}.{EventsConst.EventLevelDeleted}";
+
+        public DeleteLevelUseCase(
+            IAggregateRoot aggregateRoot,
+            ILevelRepository<TEntity> levelRepository,
+            IApplicationToDomain applicationToDomain,
+            IDomainToApplication domainToApplication
+        )
+            : base(aggregateRoot, applicationToDomain, domainToApplication)
+        {
+            _levelRepository = levelRepository;
+        }
+
+        public override async Task<DeleteLevelApplicationResponse> Handle(
+            DeleteLevelCommand request
+        )
+        {
+            var dataDeleteLevel = MapToRequestForDomain(request);
+            var level = AggregateRoot.DeleteLevel(dataDeleteLevel);
+            var response = MapToResponse(level);
+            _ = await Persistence(response);
+            EmitEvent(Channel, JsonSerializer.Serialize(response));
+            return response;
+        }
+
+        private DeleteLevelDomainRequest MapToRequestForDomain(DeleteLevelCommand request)
+        {
+            return new DeleteLevelDomainRequest { LevelId = request.LevelId };
+        }
+
+        private DeleteLevelApplicationResponse MapToResponse(DeleteLevelDomainResponse level)
+        {
+            return new DeleteLevelApplicationResponse { LevelId = level.LevelId };
+        }
+
+        private async Task<TEntity> Persistence(DeleteLevelApplicationResponse response)
+        {
+            return await _levelRepository.DeleteAsync(Guid.Parse(response.LevelId));
+        }
+    }
+}
