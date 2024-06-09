f43bfa18a0631bf8e0470236f6e833c04b301056
SebastianCamargo-ITM <juancamargo1135122@correo.itm.edu.co>
2024-06-09 12:22:50 -0500

    chore: Add appsettings files for development environment

---
+++ src/Backend/Analytics/Infrastructure/Services/Helpers/DeleteLevelHelper.cs
@@ -0,0 +1,34 @@
+using Analytics.Application.Commands;
+using Analytics.Application.Responses;
+using Analytics.Infrastructure.Services.Helpers.Base;
+using Shared.Infrastructure.ProtocolBuffers.Analytics.Requests;
+using Shared.Infrastructure.ProtocolBuffers.Analytics.Responses;
+
+namespace Analytics.Infrastructure.Services.Helpers
+{
+    internal class DeleteLevelHelper : BaseHelperServiceInfrastructure
+    {
+        public static async Task<DeleteLevelAnalyticsResponse> DeleteLevelAsync(
+            DeleteLevelAnalyticsRequest request
+        )
+        {
+            var deleteLevelCommand = MapToDeleteLevelCommand(request);
+            var data = await Application.DeleteLevel(deleteLevelCommand);
+            return MapToDeleteLevelResponse(data);
+        }
+
+        private static DeleteLevelCommand MapToDeleteLevelCommand(
+            DeleteLevelAnalyticsRequest request
+        )
+        {
+            return new DeleteLevelCommand { LevelId = request.LevelId };
+        }
+
+        private static DeleteLevelAnalyticsResponse MapToDeleteLevelResponse(
+            DeleteLevelApplicationResponse data
+        )
+        {
+            return new DeleteLevelAnalyticsResponse { LevelId = data.LevelId };
+        }
+    }
+}
